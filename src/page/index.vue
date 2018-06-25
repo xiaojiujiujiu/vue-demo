e7e1e1685a51d18fd55ced3cddf8c3d8c20a5f71
yanghui7 <yanghui7@vipkid.com.cn>
2018-06-25 16:57:41 +0800

    dev

--- src/page/index.vue
+++ src/page/index.vue
@@ -12,6 +12,22 @@
                    <p class="pic-r-b"></p>
                 </div>
             </div>
+            <section class="section-box">
+               <dl class="car-box start">
+                 <dt>关注的车</dt>
+                 <dd>28辆</dd>
+               </dl>
+               <dl class="car">
+                 <dt>精品</dt>
+                <dd>二手车</dd>
+               </dl>
+            </section>
+            <section class="list-scroll"> 
+              <div class="scroll-item" v-for="(item,index) in scrollList" :key="index">
+                <img :src="item.imgUrl" alt="">
+                <p>{{item.desc}}</p>
+              </div>
+            </section>
        </div>
   </div>
 </template>
@@ -29,6 +45,15 @@ export default {
           {id:2,imgUrl:'../../static/img/2.png'},
           {id:3,imgUrl:'../../static/img/3.png'},
       ],
+      scrollList:[
+        {id:1,imgUrl:'../../static/img/1.jpg',desc:'装备维修'},
+        {id:2,imgUrl:'../../static/img/2.jpg',desc:'查报价'},
+        {id:3,imgUrl:'../../static/img/3.jpg',desc:'查违章'},
+        {id:4,imgUrl:'../../static/img/4.jpg',desc:'物流服务'},
+        {id:5,imgUrl:'../../static/img/2.jpg',desc:'查报价'},
+        {id:6,imgUrl:'../../static/img/3.jpg',desc:'查违章'},
+        {id:7,imgUrl:'../../static/img/4.jpg',desc:'物流服务'},
+      ]
     };
   },
   components: {
